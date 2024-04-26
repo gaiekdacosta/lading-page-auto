5c038ab68762b5092dd6c2b849ff5aa9ebbde74b
João Marcelo Ravache <marceloravache@bvpinheiro.com.br>
2024-04-26 11:28:28 -0300

    feat: new area

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
-import codeImage from './assets/7ffe3f1a-464e-43d8-9f9c-e34c6508b7f0.jpg'
+import { Box, Button, Flex, Img, Input, Text, Textarea } from "@chakra-ui/react";
+import codeImage from './assets/background.jpg';
 import { FaRobot, FaWhatsapp } from "react-icons/fa";
+import { HiOutlineDocumentReport } from "react-icons/hi";
 import Card from "./components/card";
 
 function App() {
@@ -41,39 +42,98 @@ function App() {
           p='1%'
           borderRadius='2xl'
           bg='#050409'
-          flexDirection='column'>
-            <video 
-              src=""
-              controls
+          color='black'
+          flexDirection='column'
+          gap='1'>
+            <Input
+              type='text'
+              bg='whitesmoke'
+              color='black'
+              placeholder="Nome" />
+            <Input
+              type='number'
+              bg='whitesmoke'
+              placeholder="Telefone" />
+            <Input
+              type='text'
+              bg='whitesmoke'
+              placeholder="Email" />
+            <Textarea 
+              bg='whitesmoke'
             />
             <Button
               opacity='1'
               w='100%'
-              mt='3%'
+              mt='1.5%'
               bg='#2FCCDB'
+              color='black'
               _hover={{ 
-                transform:'scale(1.1)',
+                bg:'#22def0',
                 transition: 'transform 0.3s ease'
               }}>
-              <FaWhatsapp style={{ fontSize:'22px' }} /> 
               Enviar mensagem
             </Button>
         </Flex>
       </Flex>
       <Flex 
         justifyContent='center' 
-        m='2% 0 5% 0'  
+        m='5% 0 5% 0'  
         gap='3.5'
         fontWeight='semibold'>
         <Card 
-          icon={<FaRobot style={{ fontSize:'25px', color:'#2FCCDB' }} />}
-          content={"Especialista em automação de tarefas"} />
+          icon={<HiOutlineDocumentReport style={{ fontSize:'25px', color:'#2FCCDB' }} />}
+          content={"Criação e envio de relatórios automatizados"} />
+        <Card
+          icon={<FaWhatsapp style={{ fontSize:'25px', color:'#2FCCDB' }} />} 
+          content={"Chatbot de atendimento para o Whastapp"} />
         <Card
           icon={<FaRobot style={{ fontSize:'25px', color:'#2FCCDB' }} />} 
           content={"Especialista em automação de tarefas"} />
-        <Card
-          icon={<FaRobot style={{ fontSize:'25px', color:'#2FCCDB' }} />} 
-          content={"Especialista em automação de tarefas"} />
+      </Flex>
+      <Flex 
+        justifyContent='space-around'
+        mt='5% 0 5% 0'>
+        <Img 
+          h='300px'
+          src={'reportImage'} />
+        <Flex 
+          flexDirection='column' 
+          w='35%' 
+          gap='2'>
+          <Text
+            color='#2FCCDB' 
+            letterSpacing={3} 
+            fontWeight='semibold'
+            fontSize='15px'>
+            VANTAGENS
+          </Text>
+          <Text
+            fontSize='22px'
+            lineHeight={1.1}
+            fontWeight='semibold'>
+            Evite erros e execute atividades rotineiras 
+            eficientemente com facilidade, sem interrupções.
+          </Text>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, quae dolorem. 
+            Illo reiciendis minus sit ratione ducimus sunt cum ut consequatur maxime. 
+            Aperiam, consectetur laboriosam tenetur praesentium quae nostrum iusto.
+            <br />
+            <br />
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus, 
+            facilis cupiditate odio totam vitae autem voluptates sed animi quia.
+          </Text>
+          <Button 
+            bg='#2FCCDB'
+            color='black'
+            w='50%'
+            _hover={{ 
+              bg:'#22def0',
+              transition: 'transform 0.3s ease'
+            }}>
+            Entrar em contato agora
+          </Button>
+        </Flex>
       </Flex>
     </>
   )
